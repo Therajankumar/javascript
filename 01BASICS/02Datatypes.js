82d8aca2f9fbc88a386db90602833c63e02c400c
Therajankumar <kumarrajanthakur2005@gmail.com>
2024-12-27 10:33:11 +0000

    dataypes

---
+++ 01BASICS/02Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using node js not browser
+
+//code readibility should be high  
+
+let name ="rajan"
+let age =18
+let isLoggedin=false 
+let state;
+
+//number =>2 to power 53 
+//bigint 
+//string =>" "
+//boolean =>true/false 
+//null =>standalone value 
+//undefined 
+//symbol =>unique 
+
+//object 
+
+console.log(typeof undefined);//undefined 
+console.log(typeof null);//object
