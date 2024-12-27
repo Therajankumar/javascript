f7a7ec53ae25d76e174866b256d7aaaf63024371
Therajankumar <kumarrajanthakur2005@gmail.com>
2024-12-27 17:02:49 +0000

    repoupdate

---
+++ 01BASICS/03ConversionOnOperation.js
@@ -0,0 +1,70 @@
+let score ="hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let ValueInNumber =Number(score)
+//console.log(typeof(ValueInNumber);
+//console.log(ValueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//TRUE=>1;false=>0
+
+let isLoggedIn="rajan"
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1=>true;0=>false
+//""=>false
+//"rajan"=>true
+
+let somenumber=33
+
+let StringNumber=String(somenumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//***********************OPERATIONS ********* */
+
+let value =3
+let negvalue=-value
+//console.log(negvalue);
+
+let str1="hello"
+let str2=" rajan"
+
+let str3=str1+str2
+//console.log(str3);
+
+// console.log("1" + 2);           //12
+// console.log(1 + "2");          //12
+// console.log("1" + 2 + 2);       //122
+// console.log(1 + 2 + "2");        //32
+// console.log((3 + 4) * 5 % 3);     //2
+
+//console.log(true);
+
+//postfix increment 
+let x = 3;
+const y = x++;
+console.log([x,y]);
+// x is 4; y is 3
+
+
+let x2 = 3n;
+const y2 = x2++;
+console.log([x2,y2]);
+// x2 is 4n; y2 is 3n
+
+//prefix increment 
+let x3 = 3;
+const y3 = ++x3;
+console.log([x3,y3]);
+// x3 is 4; y3 is 4
+
+let x4 = 3n;
+const y4 = ++x4;
+console.log([x4,y4]);
+// x4 is 4n; y4 is 4n
